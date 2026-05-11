161668ae4ebe625fcf1870852b127acfb27206b8
Albeeertt <alberto.sanchezsoto16@gmail.com>
2026-05-11 13:07:47 +0200

    try w/parallelize and info class

--- add_utrs/main.py
+++ add_utrs/main.py
@@ -115,6 +115,7 @@ def execute_main_program():
             dict_contenido_info = {}
             for instance_info_chr_parallelize in instances_info:
                 attr_chr = vars(instance_info_chr_parallelize)
+                print(list(attr_chr.keys()))
                 delattr(attr_chr, "genes_struct")
                 dict_contenido_info.update(attr_chr)
             print("aaaaaaaaabbbbbbbbb")
